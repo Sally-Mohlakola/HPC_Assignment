24b17d6e7df98a1831133e2effbdef7f492b69c4
froggomelo <lily.demelo@outlook.com>
2026-04-30 14:51:04 +0200

    Flatten Linked Lists to array

--- problem2/craytracer/main.c
+++ problem2/craytracer/main.c
@@ -230,7 +230,7 @@ void randomSpheres(ObjectLL *world, DynamicStackAlloc *dsa) {
                                 .sphMat = MAT_CREATE_METAL_IP(material3)});
 }
 
-CFLOAT lcg(int *n) {
+CFLOAT lcg(int *n) {c
 
     static int seed;
     const int m = 2147483647;
@@ -271,11 +271,13 @@ void randomSpheres2(ObjectLL *world, DynamicStackAlloc *dsa, int n,
 
     materialGround->lambTexture.tex = c;
     materialGround->lambTexture.texType = CHECKER;
+}
 
     obj_objLLAddSphere(world,
                        (Sphere){.center = {.x = 0, .y = -1000, .z = 0},
                                 .radius = 1000,
                                 .sphMat = MAT_CREATE_LAMB_IP(materialGround)});
+    
 
     for (int a = -2; a < 9; a++) {
         for (int b = -9; b < 9; b++) {
@@ -380,45 +382,271 @@ void randomSpheres2(ObjectLL *world, DynamicStackAlloc *dsa, int n,
 }
 #undef randomFloat
 
-__global__ cuRaytracer-base(int height, int width)
-for (int l = 0; l < WIDTH * HEIGHT; l++) {
-            int j = (HEIGHT - 1) - l / WIDTH;
-            int i = l % WIDTH;
-            pcR = pcG = pcB = 0.0;
+#define MAT_LAMBERTIAN 0
+#define MAT_METAL      1
+#define MAT_DIELECTRIC 2
 
-            for (int k = 0; k < SAMPLES_PER_PIXEL; k++) {
-                CFLOAT u =
-                    ((CFLOAT)i + util_randomFloat(0.0, 1.0)) / (WIDTH - 1);
-                CFLOAT v =
-                    ((CFLOAT)j + util_randomFloat(0.0, 1.0)) / (HEIGHT - 1);
-                r = cam_getRay(&c, u, v);
+#define MAX_SPHERES   512
+#define MAX_MATERIALS 512
 
-                temp = ray_c(r, world, MAX_DEPTH);
+typedef struct {
+    vec3 center;
+    CFLOAT radius;
+    int materialIndex;
+} DeviceSphere;
 
-                pcR += temp.r;
-                pcG += temp.g;
-                pcB += temp.b;
+typedef struct {
+    int type;
+    RGBColorF albedo;
+    CFLOAT fuzz;
+    CFLOAT ir;
+} DeviceMaterial;
 
-                alloc_linearAllocFCFreeAll(lafc);
-            }
+void addCudaSphere(
+    DeviceSphere *h_spheres,
+    DeviceMaterial *h_materials,
+    int *numSpheres,
+    int *numMaterials,
+    int maxSpheres,
+    int maxMaterials,
+    vec3 center,
+    CFLOAT radius,
+    DeviceMaterial material
+) {
+    if (*numSpheres >= maxSpheres || *numMaterials >= maxMaterials) {
+        fprintf(stderr, "Scene arrays are full\n");
+        exit(1);
+    }
 
-            image[i + WIDTH * (HEIGHT - 1 - j)] =
-                writeColor(pcR, pcG, pcB, SAMPLES_PER_PIXEL);
+    int matIndex = *numMaterials;
 
-            localSteps += 1;
+    h_materials[matIndex] = material;
+    (*numMaterials)++;
 
-            if (localSteps % stepSize == stepSize - 1) {
-#pragma omp atomic
-                stepsCompleted += 1;
+    h_spheres[*numSpheres].center = center;
+    h_spheres[*numSpheres].radius = radius;
+    h_spheres[*numSpheres].materialIndex = matIndex;
+    (*numSpheres)++;
+}
 
-                if (stepsCompleted % 100 == 1) {
-#pragma omp critical
-                    printf("Progress %lu of %u (%0.2lf%%)\n", stepsCompleted,
-                           totalSteps,
-                           100.0 * (CFLOAT)stepsCompleted / totalSteps);
-                }
+void randomSpheres2_raytracer(DeviceSphere *h_spheres,
+    DeviceMaterial *h_materials,
+    int *numSpheres,
+    int *numMaterials,
+    int maxSpheres,
+    int maxMaterials,
+    int *seed) {
+
+    DeviceMaterial groundMat;
+
+    groundMat.type = MAT_LAMBERTIAN;
+    groundMat.albedo = (RGBColorF){.r = 0.4, .g = 0.4, .b = 0.4};
+    groundMat.fuzz = 0.0;
+    groundMat.ir = 1.0;
+
+    addCudaSphere(
+    h_spheres,
+    h_materials,
+    numSpheres,
+    numMaterials,
+    maxSpheres,
+    maxMaterials,
+    (vec3){.x = 0, .y = -1000, .z = 0},
+    1000,
+    groundMat
+    );
+
+    for (int a = -2; a < 9; a++) {
+        for (int b = -9; b < 9; b++) {
+            CFLOAT chooseMat = lcg(seed);
+            vec3 center = {
+                .x = a + 0.9 * lcg(seed), .y = 0.2, .z = b + 0.9 * lcg(seed)};
+
+            if (chooseMat < 0.8) {
+                // diffuse
+                RGBColorF albedo = {
+                    .r = lcg(seed) * lcg(seed),
+                    .g = lcg(seed) * lcg(seed),
+                    .b = lcg(seed) * lcg(seed),
+
+                };
+
+                DeviceMaterial mat;
+
+                mat.type = MAT_LAMBERTIAN;
+                mat.albedo = albedo;
+                mat.fuzz = 0.0;
+                mat.ir = 1.0;
+
+                addCudaSphere(
+                    h_spheres,
+                    h_materials,
+                    numSpheres,
+                    numMaterials,
+                    maxSpheres,
+                    maxMaterials,
+                    center,
+                    0.2,
+                    mat
+                );
+
+            } else if (chooseMat < 0.95) {
+                // metal
+                DeviceMaterial mat;
+
+                mat.type = MAT_METAL;
+                mat.albedo = albedo;
+                mat.fuzz = fuzz;
+                mat.ir = 1.0;
+
+                addCudaSphere(
+                    h_spheres,
+                    h_materials,
+                    numSpheres,
+                    numMaterials,
+                    maxSpheres,
+                    maxMaterials,
+                    center,
+                    0.2,
+                    mat
+                );
+
+            } else {
+                DeviceMaterial mat;
+
+                mat.type = MAT_DIELECTRIC;
+                mat.albedo = (RGBColorF){.r = 1.0, .g = 1.0, .b = 1.0};
+                mat.fuzz = 0.0;
+                mat.ir = 1.5;
+
+                addCudaSphere(
+                    h_spheres,
+                    h_materials,
+                    numSpheres,
+                    numMaterials,
+                    maxSpheres,
+                    maxMaterials,
+                    center,
+                    0.2,
+                    mat
+                );
             }
         }
+    }
+
+    DeviceMaterial mat;
+
+    mat.type = MAT_LAMBERTIAN;
+    mat.albedo = (RGBColorF){.r = 0.4, .g = 0.2, .b = 0.1};
+    mat.fuzz = 0.0;
+    mat.ir = 1.0;
+
+    addCudaSphere(
+        h_spheres,
+        h_materials,
+        numSpheres,
+        numMaterials,
+        maxSpheres,
+        maxMaterials,
+        (vec3){.x = -4, .y = 1, .z = 0},
+        1.0,
+        mat
+    );
+
+    mat.type = MAT_LAMBERTIAN;
+    mat.albedo = (RGBColorF){.r = 0.2, .g = 0.4, .b = 0.8};
+    mat.fuzz = 0.0;
+    mat.ir = 1.0;
+
+    addCudaSphere(
+        h_spheres,
+        h_materials,
+        numSpheres,
+        numMaterials,
+        maxSpheres,
+        maxMaterials,
+        (vec3){.x = -4, .y = 1, .z = -2.2},
+        1.0,
+        mat
+    );
+
+    mat.type = MAT_LAMBERTIAN;
+    mat.albedo = (RGBColorF){.r = 0.8, .g = 0.4, .b = 0.2};
+    mat.fuzz = 0.0;
+    mat.ir = 1.0;
+
+    addCudaSphere(
+        h_spheres,
+        h_materials,
+        numSpheres,
+        numMaterials,
+        maxSpheres,
+        maxMaterials,
+        (vec3){.x = -4, .y = 1, .z = 2.2},
+        1.0,
+        mat
+    );
+
+    mat.type = MAT_LAMBERTIAN;
+    mat.albedo = (RGBColorF){.r = 0.4, .g = 0.8, .b = 0.3};
+    mat.fuzz = 0.0;
+    mat.ir = 1.0;
+
+    addCudaSphere(
+        h_spheres,
+        h_materials,
+        numSpheres,
+        numMaterials,
+        maxSpheres,
+        maxMaterials,
+        (vec3){.x = -4, .y = 1, .z = -4.2},
+        1.0,
+        mat
+    );
+}
+
+__global__ void cuRaytracer_base(int HEIGHT, int WIDTH) {
+    for (int l = 0; l < WIDTH * HEIGHT; l++) {
+        int j = (HEIGHT - 1) - l / WIDTH;
+        int i = l % WIDTH;
+        CFLOAT pcR, pcG, pcB;
+        pcR = pcG = pcB = 0.0;
+
+        for (int k = 0; k < SAMPLES_PER_PIXEL; k++) {
+            CFLOAT u =
+                ((CFLOAT)i + util_randomFloat(0.0, 1.0)) / (WIDTH - 1);
+            CFLOAT v =
+                ((CFLOAT)j + util_randomFloat(0.0, 1.0)) / (HEIGHT - 1);
+            r = cam_getRay(&dC, u, v);
+
+            temp = ray_c(r, world, MAX_DEPTH);
+
+            pcR += temp.r;
+            pcG += temp.g;
+            pcB += temp.b;
+
+            alloc_linearAllocFCFreeAll(lafc);
+        }
+
+        dImage[i + WIDTH * (HEIGHT - 1 - j)] =
+            writeColor(pcR, pcG, pcB, SAMPLES_PER_PIXEL);
+
+        localSteps += 1;
+
+        __syncthreads();
+        if (localSteps % stepSize == stepSize - 1) {
+            stepsCompleted += 1;
+
+            if (stepsCompleted % 100 == 1) {
+
+
+                printf("Progress %lu of %u (%0.2lf%%)\n", stepsCompleted,
+                        totalSteps,
+                        100.0 * (CFLOAT)stepsCompleted / totalSteps);
+            }
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
 
@@ -436,7 +664,9 @@ int main(int argc, char *argv[]) {
     const int SAMPLES_PER_PIXEL = 100;
     const int MAX_DEPTH = 50;
     RGBColorU8 *image =
-        (RGBColorU8 *)malloc(sizeof(RGBColorF) * HEIGHT * WIDTH);
+        (RGBColorU8 *)malloc(sizeof(RGBColorU8) * HEIGHT * WIDTH);
+    RGBColorU8 *hImage =
+        (RGBColorU8 *)malloc(sizeof(RGBColorU8) * HEIGHT * WIDTH);
 
     CFLOAT start = omp_get_wtime();
 
@@ -456,8 +686,8 @@ int main(int argc, char *argv[]) {
     CFLOAT distToFocus = 10.0;
     CFLOAT aperture = 0.1;
 
-    Camera c;
-    cam_setLookAtCamera(&c, lookFrom, lookAt, up, 20, aspect_ratio,
+    Camera hC;
+    cam_setLookAtCamera(&hC, lookFrom, lookAt, up, 20, aspect_ratio,
                         aperture, distToFocus);
 
     Ray r;
@@ -484,7 +714,7 @@ int main(int argc, char *argv[]) {
 
     world->hrAlloc = lafc;
 
-    CFLOAT pcR, pcG, pcB;
+ # OPENMP PARALLELIZATION   
 
 #pragma omp for
         for (int l = 0; l < WIDTH * HEIGHT; l++) {
@@ -497,7 +727,7 @@ int main(int argc, char *argv[]) {
                     ((CFLOAT)i + util_randomFloat(0.0, 1.0)) / (WIDTH - 1);
                 CFLOAT v =
                     ((CFLOAT)j + util_randomFloat(0.0, 1.0)) / (HEIGHT - 1);
-                r = cam_getRay(&c, u, v);
+                r = cam_getRay(&hC, u, v);
 
                 temp = ray_c(r, world, MAX_DEPTH);
 
@@ -508,7 +738,7 @@ int main(int argc, char *argv[]) {
                 alloc_linearAllocFCFreeAll(lafc);
             }
 
-            image[i + WIDTH * (HEIGHT - 1 - j)] =
+            hImage[i + WIDTH * (HEIGHT - 1 - j)] =
                 writeColor(pcR, pcG, pcB, SAMPLES_PER_PIXEL);
 
             localSteps += 1;
@@ -535,7 +765,56 @@ int main(int argc, char *argv[]) {
 
     printf("Execution time: %lf\n", end - start);
 
-    writeToPPM(argv[1], WIDTH, HEIGHT, image);
+# CUDA PARALLELIZATION
+    cudaMalloc(&dImage, sizeof(RGBColorF) * HEIGHT * WIDTH);
+    cudaMemcpy(dImage, hImage, sizeof(RGBColorF) * HEIGHT * WIDTH, cudaMemcpyHostToDevice);
+
+    
+    cudaMalloc(&dC, sizeof(Camera));
+    cudaMemcpy(dC, &hC, sizeof(Camera), cudaMemcpyHostToDevice);
+
+    DeviceSphere *h_spheres =
+        malloc(sizeof(DeviceSphere) * MAX_SPHERES);
+
+    DeviceMaterial *h_materials =
+        malloc(sizeof(DeviceMaterial) * MAX_MATERIALS);
+
+    int numSpheres = 0;
+    int numMaterials = 0;
+
+    cudaMalloc(&d_spheres, sizeof(DeviceSphere) * world->numObjects);
+    cudaMalloc(&d_materials, sizeof(DeviceMaterial) * world->numObjects);   
+    cudaMemcpy(d_spheres, h_spheres, sizeof(DeviceSphere) * world->numObjects, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_materials, h_materials, sizeof(DeviceMaterial) * world->numObjects, cudaMemcpyHostToDevice);
+
+    randomSpheres2_cuda(
+    h_spheres,
+    h_materials,
+    &numSpheres,
+    &numMaterials,
+    MAX_SPHERES,
+    MAX_MATERIALS,
+    &seed
+    );
+
+    cudaMalloc(&d_spheres, sizeof(DeviceSphere) * numSpheres);
+    cudaMalloc(&d_materials, sizeof(DeviceMaterial) * numMaterials);
+
+    cudaMemcpy(d_spheres, h_spheres,
+            sizeof(DeviceSphere) * numSpheres,
+            cudaMemcpyHostToDevice);
+
+    cudaMemcpy(d_materials, h_materials,
+            sizeof(DeviceMaterial) * numMaterials,
+            cudaMemcpyHostToDevice);
+
+    writeToPPM(argv[1], WIDTH, HEIGHT, hImage);
+    writeToPPM(argv[1], WIDTH, HEIGHT, dImage);
+
     free(image);
+    free(hImage);
+    cudaFree(dImage);
+    cudaFree(dC);
+
     return 0;
 }
